2e5a7d44b1865c292a101ace9a1949762323c3a5
Joshua Bradford <jjyet@yahoo.com>
2017-06-24 22:55:52 -0400

    changed output to meet and exceed story https://www.pivotaltracker.com/story/show/147717075 by sending output to new output website

--- src/js/main.js
+++ src/js/main.js
@@ -3,10 +3,36 @@ import { postClick } from './ajax';
 var container = document.getElementById("container")
 
 function add(event){
-  console.log(postClick(event.clientX, event.clientY));
+  console.log(postClick(convertXtoDollars(event.clientX), convertYtoDollars(event.clientY)));
   console.log(event)
   console.log(event.clientX)
   console.log(event.clientY)
+}
+
+function convertYtoDollars(yCoord){
+  return "$" + Math.round(((yCoord - 100) / 6) * 100) / 100
+}
+
+function convertXtoDollars(xCoord){
+  if (xCoord < 82){
+    return "Clothes"
+  }
+  if (xCoord < 130){
+    return "Eating_Out"
+  }
+  if (xCoord < 181){
+    return "Entertainment"
+  }
+  if (xCoord < 236){
+    return "Gas"
+  }
+  if (xCoord < 291){
+    return "Personal Care"
+  }
+  if (xCoord < 346){
+    return "Other"
+  }
+
 }
 
 container.addEventListener('click', add);
