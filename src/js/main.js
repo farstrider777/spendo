e2d588cfaed1c59da57454b278076392f64e7393
Joshua Bradford <jjyet@yahoo.com>
2017-06-24 23:27:53 -0400

    added readout underneath as specified in story https://www.pivotaltracker.com/story/show/147788781

--- src/js/main.js
+++ src/js/main.js
@@ -1,19 +1,26 @@
 import { postClick } from './ajax';
+import $ from 'jquery';
 
-var container = document.getElementById("container")
+var container = document.getElementById('container');
+var output = document.getElementById('output');
 
 function add(event){
-  console.log(postClick(convertXtoDollars(event.clientX), convertYtoDollars(event.clientY)));
+  console.log(postClick(convertXtoCategory(event.clientX), convertYtoDollars(event.clientY)));
   console.log(event)
   console.log(event.clientX)
   console.log(event.clientY)
+  output.innerHTML = `this purchase is $${convertYtoDollars(event.clientY)}<br>
+                      you will spend $${Math.round(100 * convertYtoDollars(event.clientY) * 7) / 100} per week<br>
+                      you will spend $${Math.round(100 * convertYtoDollars(event.clientY) * 30.5) / 100} per month<br>
+                      you will spend $${Math.round(100 * convertYtoDollars(event.clientY) * 365.25) / 100} per year<br>
+                      if you maintain these spending habits`
 }
 
 function convertYtoDollars(yCoord){
-  return "$" + Math.round(((yCoord - 100) / 6) * 100) / 100
+  return Math.round(((yCoord - 100) / 6) * 100) / 100
 }
 
-function convertXtoDollars(xCoord){
+function convertXtoCategory(xCoord){
   if (xCoord < 82){
     return "Clothes"
   }
