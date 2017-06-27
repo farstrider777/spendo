f8f7032e7556e4486a6b9fa41b2cbb6d0709d02f
Joshua Bradford <jjyet@yahoo.com>
2017-06-27 13:04:24 -0400

    made modal for confirmation of purchase before sent to database to satisfy story: #147811543https://www.pivotaltracker.com/story/show/147811543

--- src/js/main.js
+++ src/js/main.js
@@ -3,14 +3,18 @@ import $ from 'jquery';
 
 var container = document.getElementById('container');
 var output = document.getElementById('output');
-var sureButton = document.getElementById('sureButton');
+var modalBackground = document.getElementById('modalBackground');
+var modalClose = document.getElementById('modalClose');
+var confirmButton = document.getElementById('confirmButton');
 var dollarPick = 0;
 var categoryPick = "none";
+
 
 function add(event){
   //console.log(postClick(convertXtoCategory(event.clientX), convertYtoDollars(event.clientY)));
   // console.log(convertYtoDollars(event.clientY))
   // console.log(event.clientY)
+  sureCheck();
   dollarPick = (convertYtoDollars(event.clientY));
   categoryPick = (convertXtoCategory(event.clientX));
 
@@ -19,6 +23,11 @@ function add(event){
                       you will spend $${Math.round(100 * dollarPick * 30.5) / 100} per month<br>
                       you will spend $${Math.round(100 * dollarPick * 365.25) / 100} per year<br>
                       if you maintain these spending habits.<br>`;
+}
+
+function postData(event){
+  console.log(postClick(convertXtoCategory(event.clientX), convertYtoDollars(event.clientY)));
+  sureCheck()
 }
 
 function convertYtoDollars(yCoord){
@@ -47,9 +56,17 @@ function convertXtoCategory(xCoord){
 }
 
 function sureCheck(event){
-  console.log(event)
-  postClick(categoryPick, dollarPick)
+  //postClick(categoryPick, dollarPick)
+  if($(".modal").hasClass("is-active")){
+     $(".modal").removeClass("is-active")
+
+  }else{
+    $(".modal").addClass("is-active")
+  }
+
 }
 
 container.addEventListener('click', add);
-sureButton.addEventListener('click', sureCheck);
+modalClose.addEventListener('click', sureCheck);
+modalBackground.addEventListener('click', sureCheck);
+confirmButton.addEventListener('click', postData);
